66c1c4f4d4be32017cab9a73b11a7b0c71ad7b13
Danish Shafi <danish.shafi@live.co.uk>
2023-12-15 15:12:27 +0000

    push to git

---
+++ app/components/Navbar/index.js
@@ -0,0 +1,76 @@
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+`;
+
+const NavLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    color: #15cdfc;
+  }
+`;
+
+// const Bars = styled(faBars)`
+//   display: none;
+
+//   @media screen and (max-width: 768px) {
+//     display: block;
+//     position: absolute;
+//     top: 0;
+//     right: 0;
+//     transform: translate(-100%, 75%);
+//     font-size: 1.8rem;
+//     cursor: pointer;
+//   }
+// `;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+`;
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/">
+          <h1>ARES</h1>
+        </NavLink>
+        <Bars />
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
